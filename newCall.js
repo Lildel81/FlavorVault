f1741ccc6e871c01c498f608fe4de995d09a6209
Lildel81 <103783285+Lildel81@users.noreply.github.com>
2024-03-11 21:31:02 -0700

    first push, new files

--- newCall.js
+++ newCall.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function getUrlParameter(name) {
   name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
   var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
@@ -259,8 +260,33 @@ async function run() {
   }
 }
 run().catch(console.dir);
+=======
+function makeApiCall(q){
+
+
+//document.getElementById('output').innerText = ("works")
+const requestOptions = {
+    method: "GET",
+    redirect: "follow"
+  };
+  
+  fetch("https://api.spoonacular.com/recipes/complexSearch?apiKey=e7ec260d55134d24ae88a19b2ae182a2", requestOptions)
+    .then((response) => response.text())
+    //.then((result) => document.getElementById("output").innerHTML="<h1>"+result[0].title+"</h1><br><img src = "+result[0].image+'"width=400/>')
+    //.then((result) => console.log(result))
+    .then((result) => document.getElementById("output").innerHTML=(result))
+    .catch((error) => console.error(error));
+    
+    
+
+
+
+>>>>>>> 12c935f (first push, new files)
 }
 
 
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 12c935f (first push, new files)
