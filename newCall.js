b004d520c8d38b4117639fc1c1437368493e5466
Lildel81 <103783285+Lildel81@users.noreply.github.com>
2024-03-14 13:52:38 -0700

    Populates picture on search. Added ability to pull query from search bar.

--- newCall.js
+++ newCall.js
@@ -1,4 +1,10 @@
-function makeApiCall(q){
+//const { parsed } = require("yargs");
+
+let res;
+
+async function makeApiCall(q, n){
+let info;
+
 
 
 //document.getElementById('output').innerText = ("works")
@@ -6,19 +12,41 @@ const requestOptions = {
     method: "GET",
     redirect: "follow"
   };
-  
-  fetch("https://api.spoonacular.com/recipes/complexSearch?apiKey=e7ec260d55134d24ae88a19b2ae182a2", requestOptions)
-    .then((response) => response.text())
-    //.then((result) => document.getElementById("output").innerHTML="<h1>"+result[0].title+"</h1><br><img src = "+result[0].image+'"width=400/>')
-    //.then((result) => console.log(result))
-    .then((result) => document.getElementById("output").innerHTML=(result))
-    .catch((error) => console.error(error));
+  try {
+  const response = await fetch("https://api.spoonacular.com/recipes/complexSearch?apiKey=e7ec260d55134d24ae88a19b2ae182a2&number=1&query="+q, requestOptions)
+    const result = await response.json();
+    //console.log(result);
     
-    
+    return result;
+  }
+    catch(error){
+      console.error(error);
+      return null;
+    }
+  } 
+    async function getResult(){
+      let q = document.getElementById('search').value;
+      res = await makeApiCall(q, 1);
+      console.log(res);
+      str_res = JSON.stringify(res);
+      console.log(str_res);
 
+      try{
+        const parsedData = JSON.parse(str_res);
+        const imageUrl = parsedData.results[0].image;
+        //const recipeName = parsedData.results[0].name;
 
+        console.log(imageUrl);
+        document.getElementById('recipeImage').src = imageUrl;
+        //document.getElementById('recipeName').innerText = recipeName;
+      } catch (error) {
+        console.error("Error parsing JSON:", error);
+      }
 
-}
+    }
+    //return info;
+//getResult();
+
 
 
 
