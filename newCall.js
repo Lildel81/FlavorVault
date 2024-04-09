d708fdb52b818bc90566154f5eb399598b5eb0b3
Lildel81 <103783285+Lildel81@users.noreply.github.com>
2024-04-08 18:26:47 -0700

    Added recipe name, Ingredients, and Instructions to the API Call

--- newCall.js
+++ newCall.js
@@ -35,6 +35,38 @@ const requestOptions = {
     }
   } 
 
+  async function makeApiCall(q, n){
+
+
+
+
+//document.getElementById('output').innerText = ("works") 
+//This is left in for testing purposes to make sure the 
+// function is getting called uncomment the line. 
+
+//Don't change this part
+const requestOptions = {
+    method: "GET",
+    redirect: "follow"
+  };
+  try {
+      //This is the call to the API. Currently there is a static 1 in the number of queries that will be displayed. To change that modify
+      //The code to this:
+      //const response = await fetch("https://api.spoonacular.com/recipes/complexSearch?apiKey=e7ec260d55134d24ae88a19b2ae182a2&number=${n}&query="+q, requestOptions)
+
+  const response = await fetch("https://api.spoonacular.com/recipes/complexSearch?apiKey=e7ec260d55134d24ae88a19b2ae182a2&number=1&query="+q, requestOptions)
+    const result = await response.json();
+    //console.log(result);
+    //this is left in for testing purposes. Uncomment to see the results in the console.
+    
+    return result;
+  }
+    catch(error){
+      console.error(error);
+      return null;
+    }
+  } 
+
   //Function to call the upper fuction and get the results. 
     async function getResult(){
       let q = document.getElementById('search').value; // gets the value of the search bar. change 'search' to whatever the ID is for your search bar.
@@ -46,22 +78,135 @@ const requestOptions = {
       try{
         const parsedData = JSON.parse(str_res);
         const imageUrl = parsedData.results[0].image;
-        //const recipeName = parsedData.results[0].name;   still working on this part.
-
+        const recipeName = parsedData.results[0].title;
+        const recipeID = parsedData.results[0].id;
+        console.log(recipeID);
+        //console.log("check point");
+        //console.log(recipeName);
         //console.log(imageUrl); // prints the URL of the image to the console. left in for testing, uncomment to use. 
+
+        
 
         document.getElementById('recipeImage').src = imageUrl; //displays the image to 'recipeImage' in the HTML. 
         // to use this in your HTML the syntax looks like this. <img id="recipeImage"></img>
         //Change "recipeImage" to whatever ID you are using.
 
 
-        //document.getElementById('recipeName').innerText = recipeName; still working on this part
+        document.getElementById('recipeName').innerText = recipeName; //still working on this part
+
+        getIngs(recipeID);
+        getIns(recipeID);
       } catch (error) {
         console.error("Error parsing JSON:", error);
       }
-
+      
     }
+  
+
+    async function getIngredients(id){
+      
+      
+      
+      
+      
+      
+      //Don't change this part
+      const requestOptions = {
+          method: "GET",
+          redirect: "follow"
+        };
+        try {
+            
+        const response = await fetch("https://api.spoonacular.com/recipes/"+id+"/ingredientWidget.json?apiKey=e7ec260d55134d24ae88a19b2ae182a2", requestOptions)
+          const ingredients = await response.json();
+          //console.log(result);
+          //this is left in for testing purposes. Uncomment to see the results in the console.
+          
+          return ingredients;
+        }
+          catch(error){
+            console.error(error);
+            return null;
+          }
+        } 
+
+        async function getIngs(recipeID){
+          let q = document.getElementById('search').value; // gets the value of the search bar. change 'search' to whatever the ID is for your search bar.
+          res = await getIngredients(recipeID);
+          //console.log(res); // prints the results on the console. left in for testing. Uncomment to use. 
+          str_res = JSON.stringify(res);
+          let ings = [];
+          
+
+          try{
+            const parsedData = JSON.parse(str_res);
+            i = 0;
+            while (parsedData.ingredients[i+1]!= null){
+            ings[i] = parsedData.ingredients[i].name;
+            
+            i++;
+            }
+            ings[i] = parsedData.ingredients[i].name;
+            console.log(ings);
+            //console.log("check point");
+            //console.log(recipeName);
+            //console.log(imageUrl); // prints the URL of the image to the console. left in for testing, uncomment to use.
+          } catch (error) {
+            console.error("Error parsing JSON:", error);
+          }
+          let ul = document.createElement('ul');
+          ings.forEach(function(item) {
+            var li = document.createElement('li');
+            li.textContent = item;
+            ul.appendChild(li);
+        });
+        document.getElementById("ingredients").appendChild(ul);
+        }
     
+
+        async function getInstructions(id){
+           //Don't change this part
+          const requestOptions = {
+              method: "GET",
+              redirect: "follow"
+            };
+            try {
+                
+            const response = await fetch("https://api.spoonacular.com/recipes/"+id+"/summary?apiKey=e7ec260d55134d24ae88a19b2ae182a2", requestOptions)
+              const result = await response.json();
+              //console.log(result);
+              //this is left in for testing purposes. Uncomment to see the results in the console.
+              //console.log(result);
+              return result;
+            }
+              catch(error){
+                console.error(error);
+                return null;
+              }
+            }
+            async function getIns(recipeID){
+              //let q = document.getElementById('search').value; // gets the value of the search bar. change 'search' to whatever the ID is for your search bar.
+              res = await getInstructions(recipeID);
+              //console.log(res); // prints the results on the console. left in for testing. Uncomment to use. 
+              str_res = JSON.stringify(res);
+              
+              
+              
+    
+              try{
+                const parsedData = JSON.parse(str_res);
+                summ = parsedData.summary;
+                console.log(summ);
+                document.getElementById("inst").innerHTML = summ;
+
+                
+              } catch (error) {
+                console.error("Error parsing JSON:", error);
+              }
+              
+
+
+            }
 //getResult(); // left in for testing. To run this uncomment and type "node getResult()"" in the terminal. 
 //be sure to uncomment the console.log lines as well so you can see what is printed. The picture will not
 //display in the terminal.
