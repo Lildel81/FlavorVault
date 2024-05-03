aec7072224049e33aecd32416815e179508404b5
MrMo21 <161257728+MrMo21@users.noreply.github.com>
2024-05-03 02:33:21 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,41 +1,28 @@
-function addToMongoDB(){
-  console.log('it is working');
-  const image = document.getElementById("recipeImage").src;
-  const name = document.getElementById("recipeName").innerText;
-  const ingredients = document.getElementById("ingredients").innerText;
-  const instructions = document.getElementById("inst").innerText;
-
-  const recipe = {
-    image: image, 
-    name: name,
-    ingredients: ingredients,
-    instructions: instructions
-  };
-  console.log(recipe);
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const expressLayoutes = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+const config = require('./startup/config');
+const winston = require('winston');
+const err = require('./middleware/errors')
+const recipeRoutes = require('./routes/recipe-routes');
+const app = express();
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const credentials = 'C:/Users/terry/Dropbox/Spring 2024/CSC 131 - Software Development/Code Workspace/Cert/X509-cert-8028175764436620696.pem'
-const client = new MongoClient('mongodb+srv://codebreakers.g8e7wjq.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&appName=codebreakers', {
-  tlsCertificateKeyFile: credentials,
-  serverApi: ServerApiVersion.v1
-});
-const arrToInsert = ['Tuna', 'Mayo', 'Relish', 'Bread'];
-async function run() {
-  try {
-    await client.connect();
-    const database = client.db("CSC131");
-    const collection = database.collection("Recipes");
-    const docCount = await collection.countDocuments({});
-    const result = await collection.insertOne({ recipe })
+require ('./startup/db')();
+require('./startup/logging')();
+require('./startup/validation')();
 
-    console.log(docCount);
-    // perform actions using client
-  } finally {
+app.use(expressLayoutes);
+app.set('view engine', 'ejs');
 
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-}
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(recipeRoutes.routes);
+app.use(err);
+
+app.listen(config.port, () => winston.info('App is listening on url http://localhost:' + config.port));
